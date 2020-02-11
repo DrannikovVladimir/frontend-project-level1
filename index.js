99255bf92b2cc0dc4adc6672b4f44aff67d5212a
Vladimir <krowen@inbox.ru>
2020-02-11 10:50:26 +0600

    Сделал общий движок

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
+const gameFlow = (gameQuestion, game) => {
   console.log('Welcome to the Brain Games!');
-  console.log('');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  console.log(`${gameQuestion}`);
+  let count = 1;
+  let isCorrect = true;
+  while (count < 4 && isCorrect) {
+    const [strExpression, correctAnswer] = game();
+    console.log(`Question: ${strExpression}`);
+    const userAnswer = readlineSync.question('You answer: ');
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      count += 1;
+      if (count > 3) {
+        console.log(`Congratulation, ${userName}`);
+      }
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
+      console.log(`Let's try again, ${userName}`);
+      isCorrect = false;
+    }
+  }
 };
 
-export default greeting;
+export default gameFlow;
