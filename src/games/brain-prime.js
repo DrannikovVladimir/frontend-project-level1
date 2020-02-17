cbc9af8c3aee895863b4e1c4393e3144001ff6d4
Vladimir <krowen@inbox.ru>
2020-02-17 14:58:11 +0600

    Переименовал количественные переменные

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,13 +4,13 @@ import getRandomNumber from '../util/util.js';
 const GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const MIN_NUMBER = 1;
 const MAX_NUMBER = 20;
-const COUNT_START = 2;
+const START_COUNT = 2;
 
 const isPrime = (num) => {
   if (num < 2) {
     return false;
   }
-  for (let i = COUNT_START; i < num / 2; i += 1) {
+  for (let i = START_COUNT; i < num / 2; i += 1) {
     if (num % i === 0) {
       return false;
     }
